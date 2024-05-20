040ba0f521cd1097d58afe68a76a944ddc262020
Felipe Barreiro <114102258+Lipebarreiro@users.noreply.github.com>
2024-05-20 20:37:37 -0300

    Add files via upload

---
+++ js/validacao.js
@@ -0,0 +1,29 @@
+function validacao() {
+    let nome = document.getElementById("i_nome");
+    let sobrenome = document.getElementById("i_sobrenome");
+    let email = document.getElementById("i_email");
+    let telefone = document.getElementById("i_telefone");
+    let data = document.getElementById("i_data");
+    let senha = document.getElementById("i_senha");
+    let con_senha = document.getElementById("i_conSenha");
+
+    estilo_input(nome, "#f45656", "#808080");
+    estilo_input(sobrenome, "#f45656", "#808080");
+    estilo_input(email, "#f45656", "#808080");
+    estilo_input(telefone, "#f45656", "#808080");
+    estilo_input(data, "#f45656", "#808080");
+    estilo_input(senha, "#f45656", "#808080");
+    estilo_input(con_senha, "#f45656", "#808080");
+}
+
+function estilo_input(input, cor_1, cor_2) {
+
+    var alert = document.getElementById("f_alert");
+
+    if (!input.checkValidity()) {
+        alert.style.display = "block";
+        return input.style.border = `3px solid ${cor_1}`;
+    } else {
+        return input.style.border = `1px solid ${cor_2}`;
+    }
+}
